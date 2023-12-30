b5370eb6962933624536e5c246dbf8a21c5c8603
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2023-12-30 14:57:27 +0100

    add Employee class

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee {
+    private String lastName;
+    // Дополнительные поля и методы класса Employee
+
+    public Employee(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
